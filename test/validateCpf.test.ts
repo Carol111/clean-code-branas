a8b623e7576faad654b780ae661541da0c788609
Caroline Carvalho <carvalhocaroline111@gmail.com>
2025-04-28 20:04:06 -0300

    adiciona teste de cpf inválido

--- test/validateCpf.test.ts
+++ test/validateCpf.test.ts
@@ -1,10 +1,19 @@
 import { validateCpf } from "../src/validateCpf";
 
-test("Deve testar um CPF válido", async () => {
+test("Deve validar um CPF", async () => {
   //given
   const cpf = "97456321558";
   //when
   const isValid = validateCpf(cpf);
   //then
   expect(isValid).toBe(true);
+})
+
+test("Não deve validar o CPF", async () => {
+  //given
+  const cpf = "11111111111";
+  //when
+  const isValid = validateCpf(cpf);
+  //then
+  expect(isValid).toBe(false);
 })
