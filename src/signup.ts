b5d34dc1a9e9a3cbb69cde4c12a599ca16b72261
Caroline Carvalho <carvalhocaroline111@gmail.com>
2025-04-29 13:53:08 -0300

    adiciona endpoint de criar conta

--- src/signup.ts
+++ src/signup.ts
@@ -1,5 +1,14 @@
-import express from "express";
+import express, { Request, Response} from "express";
+import crypto from "crypto";
 
 const app = express();
+
+app.post("/signup", async (req: Request, res:  Response) => {
+  const accountId = crypto.randomUUID();
+
+  res.json({
+    accountId
+  });
+})
 
 app.listen(3000);
