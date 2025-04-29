863438ea72df9b878d4228400339a2f808c49006
Caroline Carvalho <carvalhocaroline111@gmail.com>
2025-04-29 14:05:19 -0300

    cria objeto para salvar conta criada temporariamente

--- src/signup.ts
+++ src/signup.ts
@@ -2,9 +2,21 @@ import express, { Request, Response} from "express";
 import crypto from "crypto";
 
 const app = express();
+app.use(express.json());
+
+const accounts: any = [];
 
 app.post("/signup", async (req: Request, res:  Response) => {
+  const input = req.body;
   const accountId = crypto.randomUUID();
+  const account = {
+    accountId,
+    name: input.name,
+    email: input.email,
+    document: input.document,
+    password: input.password,
+  };
+  accounts.push(account);
 
   res.json({
     accountId
